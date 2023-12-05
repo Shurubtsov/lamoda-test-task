02b940341f64fbe1a83d860bf841f45b3fd162c7
Данила Шурубцов <dshutubtsov@gmail.com>
2023-12-05 12:12:19 +0300

    add pkgs

---
+++ pkg/client/postgresql/postgresql.go
@@ -0,0 +1,70 @@
+package postgresql
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Shurubtsov/lamoda-test-task/internal/config"
+	"github.com/Shurubtsov/lamoda-test-task/pkg/logging"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// Client Интерфейс для имплементации методов из драйвера PGX для быстродействия.
+type Client interface {
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, arguments ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, arguments ...interface{}) pgx.Row
+	Begin(ctx context.Context) (pgx.Tx, error)
+	BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error)
+}
+
+// NewClient создаёт новый клиент пула соединений pgxpool от PGX драйвера для PostgreSQL.
+func NewClient(ctx context.Context, maxAttemts int, cfg config.Config) (pool *pgxpool.Pool, err error) {
+	logger := logging.GetLogger().Logger
+
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", cfg.Storage.Username, cfg.Storage.Password, cfg.Storage.Host, cfg.Storage.Port, cfg.Storage.Database)
+	logger.Info().Str("dsn", dsn).Msg("Start connect to database")
+
+	err = doWithTries(func() error {
+		ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+		defer cancel()
+
+		pool, err = pgxpool.New(ctx, dsn)
+		if err != nil {
+			return err
+		}
+
+		// пингуем базу данных для подтверждения соединения
+		err = pool.Ping(ctx)
+		if err != nil {
+			return err
+		}
+
+		return nil
+
+	}, maxAttemts, 5*time.Second)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("Can't connect to database")
+	}
+
+	logger.Info().Msg("Connected to database")
+	return pool, nil
+}
+
+// doWithTries функция для воспроизведения нескольких попыток создать пул соединений.
+func doWithTries(fn func() error, attemts int, delay time.Duration) (err error) {
+	for attemts > 0 {
+		if err = fn(); err != nil {
+			time.Sleep(delay)
+			attemts--
+
+			continue
+		}
+		return nil
+	}
+
+	return
+}
---
+++ pkg/logging/logging.go
@@ -0,0 +1,115 @@
+package logging
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/Shurubtsov/lamoda-test-task/internal/config"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/pkgerrors"
+)
+
+type logger struct {
+	zerolog.Logger
+}
+
+var (
+	instance *logger
+	once     sync.Once
+)
+
+// GetLogger функция для получения инстанса логгера.
+func GetLogger() *logger {
+	return instance
+}
+
+func init() {
+
+	cfg := config.GetConfig()
+
+	once.Do(func() {
+
+		// управление проверкой вывода логов
+		checkLogOutput := map[string]io.Writer{
+			"develop": os.Stderr,
+			"prod":    io.Discard,
+		}
+
+		/*
+			Уровни логирования делятся на константы библиотеки от -1 до 5
+			Чем ниже уровень логгирования тем больше логов будет показано.
+
+			Таким образом при выборе уровня Trace (-1) будут показаны все остальные логи.
+			Самый высокий уровень у Fatal (5), при его выборе остальные будут отсеяны.
+		*/
+		checkLogLevel := map[int]zerolog.Level{
+			-1: zerolog.TraceLevel,
+			0:  zerolog.DebugLevel,
+			1:  zerolog.InfoLevel,
+			2:  zerolog.WarnLevel,
+			3:  zerolog.ErrorLevel,
+			4:  zerolog.FatalLevel,
+		}
+
+		// проверка места для вывода логов
+		out, ok := checkLogOutput[cfg.Logging.Output]
+		if !ok {
+			out = os.Stdout
+		}
+
+		// проверка уровня логирования
+		level, ok := checkLogLevel[cfg.Logging.Level]
+		if !ok {
+			level = zerolog.NoLevel
+		}
+		zerolog.SetGlobalLevel(level)
+
+		// стартовая настройка логгера
+		output := zerolog.ConsoleWriter{
+			Out:        out,
+			NoColor:    true,
+			TimeFormat: time.UnixDate,
+		}
+
+		/*
+			Следующие функции образуют
+			кастомные настройки формата логгирования.
+
+			Формат вывода уровня логгирования,
+			Формат сообщения,
+			Формат отображения функции где вызван лог
+		*/
+		output.FormatLevel = func(i interface{}) string {
+			return strings.ToUpper(fmt.Sprintf("(%-3s) |", i))
+		}
+		output.FormatMessage = func(i interface{}) string {
+			return fmt.Sprintf("[%s]", i)
+		}
+
+		// добавление трейсера для ошибок с вызовом ".Stack()"
+		zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+
+		zerolog.CallerMarshalFunc = func(pc uintptr, file string, line int) string {
+			short := file
+			for i := len(file) - 1; i > 0; i-- {
+				if file[i] == '/' {
+					short = file[i+1:]
+					break
+				}
+			}
+			file = short
+			return file + " -> " + runtime.FuncForPC(pc).Name() + "():" + strconv.Itoa(line)
+		}
+
+		logg := zerolog.New(output).With().Timestamp().Caller().Logger()
+		logg.Info().Msg("Getting logger")
+
+		instance = &logger{Logger: logg}
+	})
+}
